3b6b107b771af4ce9f28b6ecc5dffdba832c8051
gratias_aldi <118326921+Gratiasaldi@users.noreply.github.com>
2023-08-17 22:07:41 +0700

    Update and rename Dht/ssd1306_128x64_i2c.ino to DHT_OLED_Display.ino

---
+++ DHT_OLED_Display.ino
@@ -0,0 +1,70 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x64
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print(F("Humidity: "));
+  display.print(h);
+  display.println(F("%"));
+  display.print(F("Temperature: "));
+  display.print(t);
+  display.print(F("°C / "));
+  display.print(f);
+  display.println(F("°F"));
+  display.print(F("Heat index: "));
+  display.print(hic);
+  display.print(F("°C / "));
+  display.print(hif);
+  display.println(F("°F"));
+  display.display();
+  
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
